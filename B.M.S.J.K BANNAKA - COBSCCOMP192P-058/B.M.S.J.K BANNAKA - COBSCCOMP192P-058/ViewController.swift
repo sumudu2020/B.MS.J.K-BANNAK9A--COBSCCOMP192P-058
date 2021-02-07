9e4d18d3f305acbfd3d3b157e2993120aa6efa23
sumudu2020 <78678881+sumudu2020@users.noreply.github.com>
2021-02-07 13:56:22 +0530

    Add files via upload

---
+++ B.M.S.J.K BANNAKA - COBSCCOMP192P-058/B.M.S.J.K BANNAKA - COBSCCOMP192P-058/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  B.M.S.J.K BANNAKA - COBSCCOMP192P-058
+//
+//  Created by Janani on 2021-02-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
